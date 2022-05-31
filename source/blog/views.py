656b93f14006d88c91605574a0afea7408cf0841
Redwane <redwanehammas@gmail.com>
2022-05-31 17:24:07 +0200

    Add HTML rendering

--- source/blog/views.py
+++ source/blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+posts = [
+    {
+        "author": "CoreyMS",
+        "title": "Blog Post 1",
+        "content": "First post content",
+        "date_posted": "August 27, 2019"
+    },
+    {
+        "author": "Jane Doe",
+        "title": "Blog Post 2",
+        "content": "Second post content",
+        "date_posted": "August 28, 2019"
+    },
+]
 
 def home(request):
-    return HttpResponse("<h1>Welcome Home !</h1>")
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context=context)
 
 
 def about(request):
-    return HttpResponse("<h1>Blog About</h1>")
+    return render(request, 'blog/about.html')
